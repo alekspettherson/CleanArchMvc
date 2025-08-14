f481fd61b76fbee4126eab30481f9d97c33277f8
Aleksander Pettherson da Silva <aleksander_pettherson@hotmail.com>
2025-08-14 15:32:01 -0300

    adicionando classes dto, services e o mapper para fazer a conversão entre as classes

---
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchMvc.Application.DTOs;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetById(int? id);
+        Task<ProductDTO> GetProductCategoryAsync(int? categoryId);
+        Task Create(ProductDTO productDTO);
+        Task Update(ProductDTO productDTO);
+        Task Remove(int? id);
+    }
+}
